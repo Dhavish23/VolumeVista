83b263c9241de1c16d2055042bee39173d0c5d41
EliteSSSstar <B00140975@mytudublin.ie>
2025-02-27 00:53:29 +0000

    javaFX-intellij
    miner change for IntelliJ

---
+++ demo/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.demo to javafx.fxml;
+    exports org.example.demo;
+}
